93ae7f35c9464d43fd4c034b8e533e3f94d25917
Jagadish G <jagadish.g@outlook.com>
2018-09-18 17:01:24 +0530

    Inital project files

---
+++ README.md
@@ -0,0 +1,16 @@
+# botpress-chatbase
+Chatbase is a cloud service that offers products for designing, analyzing, and optimizing conversational experiences. This botpress module integrates your bot with chatbase analytics.
+
+## Use
+Install Via NPM
+```sh
+npm install --save botpress-chatbase
+```
+chatbase.json config file:
+```JS
+{
+    "apiKey": "<your chatbase api key>",
+    "platform": "<chatbase bot platform>",
+    "notHandledIntents": "unknown,none" // optional field, comma seperated fallback intents
+}
+```
---
+++ src/index.js
@@ -0,0 +1,102 @@
+const chatbase = require("@google/chatbase");
+
+module.exports = {
+  config: {
+    apiKey: {
+      type: "string",
+      required: true,
+      default: "",
+      env: "CHATBASE_API_KEY"
+    },
+    platform: {
+      type: "string",
+      required: true,
+      default: "unknown",
+      env: "CHATBASE_PLATFORM"
+    },
+    notHandledIntents: {
+      type: "string",
+      required: false,
+      default: "",
+      env: "CHATBASE_NOT_HANDLED_INTENTS"
+    }
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  init: async (bp, configurator, helpers) => {
+    const config = await configurator.loadAll();
+    chatbase.setApiKey(config.apiKey);
+    chatbase.setPlatform(config.platform);
+
+    async function incomingMiddleware(event, next) {
+      if (event.type != "text") {
+        next();
+        return;
+      }
+
+      const notHandledIntents = config.notHandledIntents.split(",");
+      const newMsg = chatbase.newMessage();
+
+      if (notHandledIntents.includes(event.nlu.intent.name)) {
+        newMsg.setAsNotHandled;
+      } else {
+        newMsg.setAsHandled;
+      }
+
+      newMsg
+        .setAsTypeUser()
+        .setMessage(event.text)
+        .setUserId(event.user.id)
+        .setIntent(event.nlu.intent.name)
+        .send();
+
+      next();
+    }
+
+    async function outgoingMiddleware(event, next) {
+      if (event.type != "text") {
+        next();
+        return;
+      }
+
+      const newMsg = chatbase.newMessage();
+      newMsg
+        .setAsTypeAgent()
+        .setMessage(event.text)
+        .setUserId(event.user.id)
+        .setAsHandled()
+        .send();
+
+      next();
+    }
+
+    bp.middlewares.register({
+      name: "chatbase.incoming",
+      module: "botpress-chatbase",
+      type: "incoming",
+      handler: incomingMiddleware,
+      order: 10,
+      description: "Send every incoming message to chatbase for analytics"
+    });
+
+    bp.middlewares.register({
+      name: "chatbase.outgoing",
+      module: "botpress-chatbase",
+      type: "outgoing",
+      handler: outgoingMiddleware,
+      order: 10,
+      description:
+        "Updates every outgoing message in chatbase with Intent, Handled or NotHandled information"
+    });
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  ready: async (bp, configurator, helpers) => {
+    // Your module's been loaded by Botpress.
+    // Serve your APIs here, execute logic, etc.
+
+    // eslint-disable-next-line no-unused-vars
+    const config = await configurator.loadAll();
+    // Do fancy stuff here :)
+  }
+};
